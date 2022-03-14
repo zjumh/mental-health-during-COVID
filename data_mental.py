07046a354e6492e9dea341b22e17b619959ab985
zjumh <95040783+zjumh@users.noreply.github.com>
2022-03-14 15:29:11 +0800

    Add files via upload

---
+++ data_mental.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+data_path = '/data_8t/lmh_total/data_clean/'
+re_path = '/data_8t/lmh_total/data_mental'
+write_path = '/data_8t/lmh_total/mental_total/'
+write_path1 = '/data_8t/lmh_total/data_mental/'
+name =[]
+for file in os.listdir(re_path):
+    name.append(file)
+print(name)
+for file in os.listdir(data_path):
+    file_path = os.path.join(data_path, file)
+    if file in name:
+        continue
+    print(file)
+    data = pd.read_csv(file_path, low_memory=False, lineterminator="\n")
+# select the tweets containing keywords in subgroups(anxiety,depression,sleep, abuse)
+    data['anxiety'] = data['full_text'].str.contains("anxiety | anxious | antsy | restlessness | concern about the future | dread fear | nervousness | panic | panic attacks | paranoia | worry", na=False, case=False)
+    data['depression'] = data['full_text'].str.contains("collapse | anger | compassion | shame | depressed | escape | loneliness | darkness | sensitivity | lupus | autism | jealousy | paranoia | stress | postpartum | hysteria | asthma | narcissism | heroin | pain mood | aggression | immature | ruthless | insecurity | mirage | cheating | hidden | pulse | projection | sway | negative affect | affect lability | emotional instability | emotional stories | emotional support | aggressiveness | snappy | agitated | irritable mood | altered mood | mood changes | mood swing | anger | frustration | apathy | loss of interest | indifference | bliss | elation | euphoria | warm fuzziness | calmness | depressed mood | depression | down feelings | feeling low | melancholy | miserable | sadness | feeling empty | fussiness | hopelessness | self-confidence | stress | suicidal ideation", na=False, case=False)
+    data['sleep'] = data['full_text'].str.contains("abnormal dreams | nightmares | vivid dreams | asthenia | disturbed sleep | insomnia | sleep problems | early awakening | drowsiness | energy | lassitude | listlessness | exhaustion | fatigue | lethargic | sluggishness | somnolence | tiredness | weariness | hypersomnia ", na=False, case=False)
+    data['abuse'] = data['full_text'].str.contains("substance dependence | physical dependence | psychological dependence | harmful use | withdrawal state | addiction | addictive behaviour | reinforcing | rewarding | psychoactive substances | depressants | stimulants | alcohol and drug dependence | abstinent | heavy drink | drinker | alcoholism | alcohol intake | alcohol misuse | alcohol consumption | alcohol addiction | destroyer | trippy | alcohol dependence | alcoholic| risky drinking | harmful drinking | chronic alcohol use | drinking behavior | behavior impairment | chronic alcoholic intoxication | problem alcohol use | alcohol-meat | drinking for pleasure | avoidance behaviors | occupational impairment | marijuana addict | cocaine addict | heroin addict | cannabis addict | hallucinogen | opioid abuse | war on drugs | illicit use of drugs | drug affliction | narcotics abuse | drug cues | drug-seeking | weakening self-regulation | drug abuse vulnerability | drug addiction | prescription drug abuse | drug habituation | substance-induced | drug misuse | ecstasy | illicit drug | chronic drug problem | drug involvement | drug reinforcement | drug relapse | rehab | rehabilitation | nicotine | harmful smoking | cigarette smoking | tobacco consumption | tobacco chewing | alternative tobacco product | dual-tobacco use | poly-tobacco use | tobacco dependent | electric cigarette use | vape pens | e-pens | e-hookah and vape sticks | smokeless tobacco | hookah | cigars | cigarillos and little cigars | daily smoking | tobacco use disorder | quitting smoking | internet gaming disorders | gaming disorder | internet addiction | social media addictions | smartphone addiction | attention deficit disorder | problematic online gaming | disordered gamer | gaming craving | video game addiction | internet game disorder | maladaptive player game | problem internet use | pathological game use | online game genres | video-game exposure | game addiction | recreational game user | addictive game behaviors | gaming community | gain ability to challenge and dominate others | acquire status and power | immersion | escape real life | gambling disorder | pathological gambling | gambling harm| problem gambling | problematic online gambling | disordered gamblers | compulsive gambling | gambling craving | silent addiction | problem gambler | gambling behavior | preoccupation with gambling | chronic gambler | gambling addiction | gamble harm up | mounting losses | gambling-motivated crime | at-risk gambling | persistent gambling | recurrent gambling | gambling-related cognitive distortion | uncontrolled gambling | compulsive gambling | internet addiction | problematic use | problematic phone use | social media addiction | addiction ", na=False, case=False)
+    data.to_csv(write_path + file, index=False)
+    data1 = data[data['anxiety'] | data['depression'] | data['sleep'] | data['abuse']]
+    data1.to_csv(write_path1+file, index=False)
---
+++ model.py
@@ -0,0 +1,101 @@
+import os
+# import wget
+from datetime import datetime
+# from dateutils import timedelta
+# import itertools
+import pandas as pd
+import numpy as np
+# import math
+from pathlib import Path
+# import regex as re
+# from functools import reduce
+from ast import literal_eval
+from pprint import pprint
+import gensim
+from gensim.corpora import Dictionary
+from wordcloud import WordCloud
+from pickle import load, dump
+import gensim
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import matplotlib.ticker as ticker
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
+import scipy.stats
+import gensim
+from gensim.parsing.preprocessing import STOPWORDS
+from gensim.models.phrases import Phrases, Phraser
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel, LdaMulticore, LsiModel
+
+
+# Topic modeling
+df = pd.read_csv("/data_8t/lmh_total/model_merge/mental_token.csv", keep_default_na=False)
+
+# convert strings of lists to lists
+df.tokens = df.tokens.apply(literal_eval)
+
+corpus = df.tokens.values.tolist()
+dictionary = Dictionary(corpus)
+# changing these numbers can increase/decrease the run time if needed, but too exclusive will lead to worse results
+no_below = 5
+dictionary.filter_extremes(no_below=no_below, no_above=0.5)
+corpus = [dictionary.doc2bow(tokens) for tokens in corpus]
+print('vocab size: {}'.format(len(dictionary)))
+print('documents in corpus: {}'.format(len(corpus)))
+
+os.makedirs(f"/data_8t/lmh_total/model_merge/Models/{no_below}/", exist_ok=True)
+savefile = f'/data_8t/lmh_total/model_merge/Models/{no_below}/all.PICKLE'
+print('saving dataset to {}...'.format(savefile))
+dump({'corpus': corpus, 'dictionary': dictionary}, open(savefile, 'wb+'))
+loaddict = {'corpus': corpus, 'dictionary': dictionary}
+
+#topic model
+def topic_modeling(num_topics=5):
+    np.random.seed(0)
+    temp = dictionary[0]  # This is only to "load" the dictionary.
+    id2word = dictionary.id2token
+    iterations = 50
+    passes = 5
+
+    print('topics: {}'.format(num_topics))
+    print('interations: {}'.format(iterations))
+    print('passes: {}'.format(passes))
+    print('vocab size: {}'.format(len(dictionary)))
+    print('documents in corpus: {}'.format(len(corpus)))
+
+    model_directory = f"/data_8t/lmh_total/model_merge/Models/{no_below}/"
+    os.makedirs(model_directory, exist_ok=True)
+    model_name = f"{model_directory}/all_p{passes}_i{iterations}_t{num_topics}"
+    print("Model: ", model_name)
+
+    ##Create new model with desired parameters
+    # https://radimrehurek.com/gensim/models/ldamulticore.html
+    model = LdaModel(
+        corpus=corpus,  # leave commented out for batch training, uncomment to train on full corpus at once
+        id2word=id2word,
+        iterations=iterations,
+        passes=passes,
+        num_topics=num_topics,
+        random_state=0
+    )
+
+    top_topics = model.top_topics(corpus)
+
+    # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    print('\nAverage topic coherence: %.4f.' % avg_topic_coherence)
+    # pprint(top_topics)  # prints list of ((list of top probability,term tuples), topic coherence) tuples
+
+    print(datetime.now())
+    try:
+        print('saving model...')
+        model.save(model_name)
+        print('model saved as {}.'.format(model_name))
+    except Exception as e:
+        print('saving error: {}'.format(e))
+    print("----------------", "\n")
+
+n_topics = [6,7,8,9, 10, 11, 12, 13,14,15]
+for num_topics in n_topics:
+    topic_modeling(num_topics=num_topics)
